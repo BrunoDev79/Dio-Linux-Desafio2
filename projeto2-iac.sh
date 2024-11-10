7631c98795667644b05d517cef8e551b2470f8ea
BrunoDev79 <bruno.estudos97@gmail.com>
2024-11-10 13:17:01 +0000

    projeto2

---
+++ projeto2-iac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Atualizando o Sistema"
+
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando o Apache"
+apt-get install apache2 -y
+
+echo "Instalando o Unzip"
+apt-get install unzip -y
+
+echo "Baixando e copiando aplicação Dio"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip /tmp
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html
+
+
